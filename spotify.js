b9047a4e6750e91efc4e0229a44a80f61a4502d4
Mary R Ray <32748882+GitHub4Phils@users.noreply.github.com>
2018-04-24 19:33:49 -0700

    updated spotify key
    erased for secrty

--- spotify.js
+++ spotify.js
@@ -1,6 +1,6 @@
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
-	id: 	'bdac1578fcc84539a5afd24b2eae5d9c',
-	secret: '13b94d1b3cf544219b28f6e9db3f7d34'
+	id: 	'',
+	secret: ''
 	});
 module.exports = spotify;
